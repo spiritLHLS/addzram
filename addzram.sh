bbd05e17003f6095ebf1cca73715b8d8aba8429c
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-27 21:09:41 +0800

    Update addzram.sh

--- addzram.sh
+++ addzram.sh
@@ -30,39 +30,42 @@ check_root() {
 }
 
 add_zram() {
-  if [ -e "$zram_device" ]; then
-    echo "ZRAM device $zram_device exists."
-    echo "ZRAM 设备 $zram_device 已存在。"
-  else
-    _green "Please configure and add zram with the size of half the available memory!"
-    _green "请输入需要添加的zram，建议为内存的一半！"
-    _green "Please enter the zram value in megabytes (MB) (leave blank and press Enter for default, which is half of the memory):"
-    reading "请输入zram数值，以MB计算(留空回车则默认为内存的一半):" zram_size
-    if [ -z "$zram_size" ]; then
-      total_memory=$(free -m | awk '/^Mem:/{print $2}')
-      zram_size=$((total_memory / 2))
-    fi
-    if ! lsmod | grep -q zram; then
-      echo "Loading zram module..."
-      modprobe zram
-    fi
-    if ! command -v zramctl >/dev/null; then
-      _yellow "zramctl command not found. Please make sure zramctl is installed."
-      exit 1
-    fi
-    if ! command -v mkswap >/dev/null || ! command -v swapon >/dev/null; then
-      _yellow "mkswap or swapon command not found. Please make sure these commands are installed."
-      exit 1
-    fi
-    modprobe zram
-    zramctl --find --size "${zram_size}MB" --algorithm zstd
-    # zramctl /dev/zram0 --algorithm zstd --size "${zram_size}M"
-    mkswap /dev/zram0
-    swapon --priority 100 /dev/zram0
-    echo "ZRAM setup complete. ZRAM device /dev/zram0 with size ${zram_size}M and algorithm zstd is now active as swap."
-    echo "ZRAM 设置成功，ZRAM 设备路径为 /dev/zram0 大小为 ${zram_size}M 同时 zstd 已激活成为swap的一部分"
-    check_zram
+  modprobe zram
+  if [ $? -ne 0 ]; then
+    _yellow "Not find zram module, please install it with zram-tools or etc."
+    exit 1
   fi
+  _green "Please configure and add zram with the size of half the available memory!"
+  _green "请输入需要添加的zram，建议为内存的一半！"
+  _green "Please enter the zram value in megabytes (MB) (leave blank and press Enter for default, which is half of the memory):"
+  reading "请输入zram数值，以MB计算(留空回车则默认为内存的一半):" zram_size
+  if [ -z "$zram_size" ]; then
+    total_memory=$(free -m | awk '/^Mem:/{print $2}')
+    zram_size=$((total_memory / 2))
+  fi
+  if ! command -v zramctl >/dev/null; then
+    _yellow "zramctl command not found. Please make sure zramctl is installed."
+    exit 1
+  fi
+  if ! command -v mkswap >/dev/null || ! command -v swapon >/dev/null; then
+    _yellow "mkswap or swapon command not found. Please make sure these commands are installed."
+    exit 1
+  fi
+  if [ -d "/sys/block/zram0" ]; then
+    zramctl /dev/zram0 --algorithm zstd --size "${zram_size}M"
+  else
+    zramctl --find --size "${zram_size}MB" --algorithm zstd
+  fi
+  if [ $? -ne 0 ]; then
+    echo "ZRAM device $zram_device exists, cannot be duplicated, if you need to reset, please select ‘Remove zram’ first."
+    echo "ZRAM 设备 $zram_device 已存在，无法重复设置，如需重新设置请先选择删除"
+    exit 1
+  fi
+  mkswap /dev/zram0
+  swapon --priority 100 /dev/zram0
+  echo "ZRAM setup complete. ZRAM device /dev/zram0 with size ${zram_size}M and algorithm zstd is now active as swap."
+  echo "ZRAM 设置成功，ZRAM 设备路径为 /dev/zram0 大小为 ${zram_size}M 同时 zstd 已激活成为swap的一部分"
+  check_zram
 }
 
 del_zram() {
